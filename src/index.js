bbedba443037e813b3476a4efe815c22fc9ecd9a
alex <sytnikov.zp@gmail.com>
2024-02-15 20:37:46 +0200

    Fix "<=0" & " " value

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 'use strict';
 
-function factorial(number) {
-  if (number === 1 || number === 0) {
+function factorial(number = 0) {
+  if (number === 1 || number === 0 || number <= 0) {
     return 1;
   }
   return factorial(number - 1) * number;
 }
 
-console.log(factorial(5));
+console.log(factorial());
